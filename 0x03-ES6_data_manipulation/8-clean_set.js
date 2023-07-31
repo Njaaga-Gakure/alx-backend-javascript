32e4e3d9826b6081288d74f37c4458f3d088147f
Njaaga-Gakure <njaagagakure@gmail.com>
2023-07-31 20:32:06 +0000

    Check if elements of the array of strings are truthy values

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,18 @@
 export default function cleanSet(set, startString) {
   if (startString) {
     return [...set]
-      .filter((item) => item.startsWith(startString))
-      .map((item) => item.replace(startString, ''))
+      .filter((item) => {
+        if (item) {
+          return item.startsWith(startString);
+        }
+        return '';
+      })
+      .map((item) => {
+        if (item) {
+          return item.replace(startString, '');
+        }
+        return '';
+      })
       .join('-');
   }
   return '';
